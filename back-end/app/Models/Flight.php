1f5b72e9a94af06c2961ad53407c7a18720c4d91
Youssefboulahnya <flexn368@gmail.com>
2025-11-22 22:26:45 +0100

    Updating the flights controller file

--- back-end/app/Models/Flight.php
+++ back-end/app/Models/Flight.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
 {
-    //
+    // Specify the primary key if it's not the default 'id'
+    protected $primaryKey = 'ID_flight';
+
+    // The attributes that are mass assignable
+    protected $fillable = [
+        'origin',
+        'destination',
+        'temps_aller',
+        'temps_arriver',
+        'seats',
+        'price',
+        'status',
+        'created_by', // foreign key to Admin
+    ];
+
+    // If you don't want Laravel to auto-manage timestamps (created_at, updated_at)
+    public $timestamps = true; // or false if you don't have those columns
 }
