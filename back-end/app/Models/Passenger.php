4524c48ebb2ff45c83eec7e67f7502571de1968b
Youssefboulahnya <flexn368@gmail.com>
2025-11-27 03:26:34 +0100

    Final fixing of the prix_total

--- back-end/app/Models/Passenger.php
+++ back-end/app/Models/Passenger.php
@@ -56,16 +56,16 @@ class Passenger extends Model
     $basePrice = (float) $this->ticket->flight->price;
     $finalPrice = $basePrice;
 
-    // Adult vs Child pricing
+    // 
     if ($this->type === 'adult') {
         // Base adult markup: 50% of base price
         $finalPrice = $basePrice + ($basePrice * 0.5);
 
         // Extra depending on ticket class
         if ($this->ticket->classe === 'business') {
-            $finalPrice += $basePrice * 0.15;
+            $finalPrice += $finalPrice * 0.15;
         } elseif ($this->ticket->classe === 'economy') {
-            $finalPrice += $basePrice * 0.05;
+            $finalPrice += $finalPrice * 0.05;
         }
     } elseif ($this->type === 'child') {
         // Children always pay base price
